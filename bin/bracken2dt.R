8d690d9f7751dad37b8dfa51000e6d8e5b85de1d
Angel Angelov <aangeloo@gmail.com>
2020-04-20 20:09:43 +0200

    improve Rscript for better HTML table

--- bin/bracken2dt.R
+++ bin/bracken2dt.R
@@ -7,12 +7,31 @@ library(dplyr)
 library(DT)
 
 arg <- commandArgs(trailingOnly = TRUE)
+taxid_string <- "<a href='https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id="
+# <a href="http://rstudio.com">RStudio</a>
 
 bracken2dt <- function(path, outfile) {
-  dt <- fread(path) %>% 
-        arrange(desc(fraction_total_reads)) %>% 
-        DT::datatable()
-  DT::saveWidget(dt, file = outfile)
+  fread(path) %>% 
+    dplyr::arrange(desc(fraction_total_reads)) %>% 
+    dplyr::select(c(1,2,6,7)) %>%
+    dplyr::mutate(taxonomy_id = paste(taxid_string, taxonomy_id, "'>", taxonomy_id, "</a", sep = "") ) %>% # make html links to tax db
+  
+    DT::datatable(class = 'hover row-border order-column', 
+                  escape = FALSE, # needed for the html links
+                  colnames = c("Species", "Taxonomy_ID", "Estimated reads" , "Fraction of total reads"),
+                  extensions = 'Buttons',
+                  options = list(searchHighlight = TRUE, 
+                                 dom = 'Brtip', 
+                                 buttons = c('copy', 'csv', 'excel'),
+                                 pageLength = 12), 
+                  filter = 'top', 
+                  selection = 'single', 
+                  caption = paste("Bracken abundance estimation for", 
+                                  tools::file_path_sans_ext(basename(outfile) )
+                                  ) 
+                  ) %>%
+    formatPercentage(4) %>%
+    DT::saveWidget(file = outfile)
   }
 
 
