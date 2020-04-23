2f41a3547b412cc3e3e8a75f4c2dee33133741a0
Angel Angelov <aangeloo@gmail.com>
2020-04-23 19:47:32 +0200

    add heatmap output to bracken, add heatmaply to docker image also

--- bin/bracken2summary.R
+++ bin/bracken2summary.R
@@ -8,13 +8,14 @@ library(data.table)
 library(dplyr)
 library(tidyr)
 library(DT)
-#library(apexcharter) # too complicated, not used, use heatmaply instead?
+library(heatmaply)
+#library(apexcharter) # too complicated, not used, use heatmaply instead
 
 arg <- commandArgs(trailingOnly = TRUE)
 
 bracken2summary <- function(files) {
   names(files) <- basename(tools::file_path_sans_ext(files))
-  # this is the data used for apexcharter
+  # this is the tidy data
   df <- lapply(files, fread) %>%
       bind_rows(.id = "id") %>% 
       dplyr::select(c(1,2,8)) %>% 
@@ -26,36 +27,41 @@ bracken2summary <- function(files) {
       tidyr::pivot_wider(names_from = id, 
                        values_from = fraction_total_reads, 
                        values_fill = list(fraction_total_reads = 0))  # fill 0 if no value there
-  dfw %>% fwrite("bracken_summary.csv")
+  dfw %>% fwrite("bracken_summary_wide.csv")
   
+  # top 10 species for each sample, saved as wide
+  dfw_top10 <- df %>%
+    group_by(id) %>%
+    top_n(10, fraction_total_reads) %>%
+    tidyr::pivot_wider(names_from = id, 
+                       values_from = fraction_total_reads, 
+                       values_fill = list(fraction_total_reads = 0))
+  #----
   # make and save datatable widget 
   # only if samples <= 6 
   if(length(files) <= 6) {
-  dfw %>%
-    datatable(class = 'hover row-border order-column', 
-              extensions = 'Buttons',
-              options = list(searchHighlight = TRUE, 
+    dfw %>%
+      datatable(class = 'hover row-border order-column', 
+                extensions = 'Buttons',
+                options = list(searchHighlight = TRUE, 
                              dom = 'Brtip', 
                              buttons = c('copy', 'csv', 'excel'),
                              pageLength = 20), 
-              filter = 'top', 
-              selection = 'single') %>% 
-    formatPercentage(c(2,3), digits = 2) %>%
-    DT::saveWidget(file = "bracken_summary_table.html") 
+                filter = 'top', 
+                selection = 'single') %>% 
+      formatPercentage(c(2,3), digits = 2) %>%
+      DT::saveWidget(file = "bracken_summary_table.html")
     }
+  
   #----
-  # make and save apex chart (top10 species only)
-  # only if samples <= 6 
-  # df %>% 
-  #   dplyr::top_n(10, fraction_total_reads) %>%
-  #   apex(type = "bar", aes(id, fraction_total_reads, fill = name)) %>% 
-  #   ax_chart(stacked = TRUE, stackType = "100%") %>% 
-  #   ax_legend(horizontalAlign = "left", 
-  #             position = "bottom",
-  #             itemMargin = list(horizontal =5, vertical = 0)) %>%
-  #   ax_colors(scales::brewer_pal(palette = "Set2")(8)) %>%
-  #   ax_dataLabels(enabled = TRUE) %>%
-  #   htmlwidgets::saveWidget(file = "bracken_summary_plot.html")
+  # make and save heatplot for top10 taxa for samples >=2 and < 24
+  if(length(files) >= 2 & length(files) <= 24) {
+    data.frame(row.names = dfw_top10$name, dfw_top10[,-1]) %>% 
+      heatmaply(colors = hcl.colors(100, palette = "Grays", rev = T), 
+                show_dendrogram = c(F,T), 
+                file = "bracken_summary_heatmap.html"
+      )
+    }
   
 }
 # execute the function
